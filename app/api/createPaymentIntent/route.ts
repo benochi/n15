d98e87d7d8879377b77f79a0e34d5d120b562dc1
Dan Polityka <73758508+benochi@users.noreply.github.com>
2025-02-19 15:56:51 -0600

    add payment intents and api version.

--- app/api/createPaymentIntent/route.ts
+++ app/api/createPaymentIntent/route.ts
@@ -1,14 +1,28 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PaymentData, PaymentSchema } from "@/schemas/PaymentSchema";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2025-01-27.acacia", // Ensure this matches your Stripe account version
+});
 
 export async function POST(request: NextRequest){
   try {
     const body = await request.json()
     const parsedData: PaymentData = PaymentSchema.parse(body)
     console.log(parsedData) //Handle this as you see fit.
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: parsedData.amount,
+      currency: parsedData.currency || "usd", 
+      automatic_payment_methods: { enabled: true },
+    });
+
+    return NextResponse.json({ clientSecret: paymentIntent.client_secret });
   }catch(error) {
-    
+    console.error("Internal Error: ", error);
+    return NextResponse.json(
+      { error: `Internal Server Error: ${error}` },
+      { status: 500}
+    )
+
   }
 }
